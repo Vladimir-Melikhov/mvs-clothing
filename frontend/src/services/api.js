34da9094dc515ea31f262de9d78bea18fa0c39d3
Владимир Мелихов <b1esssed1@yandex.com>
2025-10-20 12:04:06 +0300

    version5

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import router from '@/router'
 
-const API_BASE_URL = 'http://localhost:8000/api/v1'
+const API_BASE_URL = 'http://localhost:8001/api/v1'
 
 // Create axios instance with default config
 const apiClient = axios.create({
